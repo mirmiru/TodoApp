b54dda95af190fdc4db36216e4c7715a2c2a67d7
Milja <mmcvirtanen@gmail.com>
2018-01-30 15:18:46 +0100

    Initial Commit

---
+++ TodoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodoList
+//
+//  Created by Milja Virtanen on 2018-01-30.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TodoList
+//
+//  Created by Milja Virtanen on 2018-01-30.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
